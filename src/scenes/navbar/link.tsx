66a71a5b913fc0561d8e77933950ca533aa24f72
MichaelBMoss <michaelbmoss@gmail.com>
2024-04-29 13:34:56 -0700

    enhance: Expand Link component with dynamic styling and props
    Enhanced the Link component by adding dynamic class assignment based on the selected page, utilizing the 'selectedPage' prop. Introduced 'setSelectedPage' to manage state changes on click, and implemented logic to convert page names to anchor-friendly IDs by removing spaces and converting to lowercase. Also added smooth transition effects on hover.

--- src/scenes/navbar/link.tsx
+++ src/scenes/navbar/link.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
-type Props = {}
-
-function link({}: Props) {
-  return (
-    <div>link</div>
-  )
+type Props = {
+  page: string;
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
 }
 
-export default link
+const Link = ({
+  page,
+  selectedPage,
+  setSelectedPage,
+}: Props) => {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "")
+
+  return (
+    <AnchorLink
+    className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
+    transition duration-500 hover:text-primary-300
+    `}
+    href={`#${lowerCasePage}`}
+    onClick={}
+    >
+      {page}
+    </AnchorLink>  )
+}
+
+export default Link
