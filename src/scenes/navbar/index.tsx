c23e147e35adfb867e85407edefa7574884312a3
MichaelBMoss <michaelbmoss@gmail.com>
2024-05-02 16:04:21 -0700

    Fix file naming and update props usage in Navbar
    - Renamed link.tsx to Link.tsx to correct casing inconsistency
    - Added selectedPage and setSelectedPage to props destructuring in Navbar component for direct usage

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
+import Link from "./Link";
 
 type Props = {
   selectedPage: string;
   setSelectedPage: (value: string) => void;
 };
 
-const Navbar = (props: Props) => {
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
     const flexBetween = "flex items-center justify-between";
 
     return <nav>
@@ -44,7 +45,7 @@ const Navbar = (props: Props) => {
                 />
               </div>
               <div className={`${flexBetween} gap-8`}>
-                </p>Sign In</p>
+                <p>Sign In</p>
                 <button>Become a Member</button>
               </div>
             </div>
