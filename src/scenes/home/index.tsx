f67379c0fea1c098ca86341a5e249ef9fa9ea544
MichaelBMoss <michaelbmoss@gmail.com>
2024-05-08 20:06:50 -0700

    [WIP] Home component
      - Create basic div structure

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,10 +1,50 @@
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { SelectedPage } from "@/shared/types";
+import ActionButton from "@/shared/ActionButton";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
+import SponsorFortune from "@/assets/SponsorFortune.png";
+
 type Props = {
-    
+  setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Home = ({}: Props) => {
+const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+
   return (
-    <div>Home</div>
+    <section
+      id="home"
+      className="gap-16 bg-gray-20 py-10 md:h-full"
+    >
+      {/* IMAGE AND MAIN HEADER */}
+      <div>
+        {/* MAIN HEADER */}
+        <div>
+          {/* HEADINGS */}
+          <div>
+            <div>
+              <div>
+                <img alt="home-page-text" src={HomePageText} />
+              </div>
+            </div>
+
+            <p>
+              roin porttitor diam nec cursus pellentesque. Sed tincidunt feugiat sapien, in sodales nisi consectetur eu. Fusce ultricies pretium cursus. Sed nec enim in mi fringilla semper. Nulla sed orci sit amet magna aliquet ultricies. 
+            </p>
+          </div>
+
+          {/* ACTIONS */}
+          <div>
+          </div>
+        </div>
+        {/* IMAGE */}
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
