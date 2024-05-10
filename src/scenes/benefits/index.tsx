42b91e848a6248ecb4208f76de498251da931e3b
MichaelBMoss <michaelbmoss@gmail.com>
2024-05-10 16:52:13 -0700

    [WIP] Benefits component
    - Create `Benefit` component to be used in `benefits/index.tsx`
    - In index.tsx create `Benefits` and map data into `Benefit` components to construct the benefits section
    - Style Benefit component and benefits section

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,7 +1,30 @@
 import HText from "@/shared/HText";
-import { SelectedPage } from "@/shared/types"
+import { BenefitType, SelectedPage } from "@/shared/types"
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid"
 import { motion } from "framer-motion";
+import Home from "../home";
+import Benefit from "./Benefit";
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description:
+      "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description:
+      "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+  },
+];
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -25,7 +48,20 @@ const Benefits = ({ setSelectedPage }: Props) => {
             care into each and every member.
           </p>
         </div>
+
+        {/* BENEFITS */}
       </motion.div>
+        <div className="md:flex items-center justify-between gap-8 mt-5">
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </div>
 
     </section>
   )
