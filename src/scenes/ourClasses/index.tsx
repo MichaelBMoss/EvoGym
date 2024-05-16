c437b90f2132252564892f304b2156ddad01d9d2
MichaelBMoss <michaelbmoss@gmail.com>
2024-05-15 22:28:49 -0700

    style: Reduce padding above OurClasses section

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const OurClasses = ({ setSelectedPage }: Props) => {
   return (
-    <section id="ourclasses" className="w-full bg-primary-100 py-40">
+    <section id="ourclasses" className="w-full bg-primary-100 pt-20 pb-40">
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
       >
