a98157127cd3ceffd0891e76ef4be7fdab998657
MichaelBMoss <michaelbmoss@gmail.com>
2024-05-15 22:07:36 -0700

    [WIP] Continue OurClasses Component
      - Add and style header section

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,9 +1,36 @@
-type Props = {}
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import HText from "@/shared/HText";
 
-const OurClasses = (props: Props) => {
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const OurClasses = ({ setSelectedPage }: Props) => {
   return (
-    <div>OurClasses</div>
-  )
-}
+    <section id="ourclasses" className="w-full bg-primary-100 py-40">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+      >
 
-export default OurClasses
+        {/* HEADER */}
+        <div className="mx-auto w-5/6">
+          <div className="md:w-3/5">
+            <HText>OUR CLASSES</HText>
+            <p className="py-5">
+              Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+              tellus quam porttitor. Mauris velit euismod elementum arcu neque
+              facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+              enim mattis odio in risus nunc.
+            </p>
+          </div>
+        </div>
+        <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default OurClasses;
