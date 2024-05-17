fea736a1ab31e3012af98064bd949a66ac2c8629
MichaelBMoss <michaelbmoss@gmail.com>
2024-05-16 21:12:56 -0700

    [WIP] Continue: Contact us component
      - Add onViewportEnter event handler to Contact Us section. This
      Updates the selected page state to highlight the Contact Us link in
      the navbar when the section enters the viewport
    
      - Add header section with styling to contactUs/index.tsx

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,9 +1,31 @@
-type Props = {}
+import HText from "@/shared/HText";
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
 
-const ContactUs = (props: Props) => {
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
   return (
-    <div>ContactUs</div>
-  )
-}
+    <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+      >
+        {/* HEADER */}
+        <div>
+          <HText>
+            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+          </HText>
+          <p className="my-5">
+            Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl
+            sapien vel rhoncus. Placerat at in enim pellentesque. Nulla
+            adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
+          </p>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
