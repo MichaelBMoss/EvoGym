2786579553e02ee438f8e84c44c8221da9a5f268
MichaelBMoss <michaelbmoss@gmail.com>
2024-05-17 01:53:16 -0700

    [WIP] Continue: Contact us component
      - Add and style ContactUsPageGraphic.png and evolve text below form

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -2,6 +2,7 @@ import HText from "@/shared/HText";
 import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
 import { useForm } from "react-hook-form";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -111,7 +112,15 @@ const ContactUs = ({ setSelectedPage }: Props) => {
               </button>
             </form>
           </div>
-
+            <div className="relative mt-16 basis-2/5 md:mt-0">
+              <div className="w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext">
+                <img
+                  className="w-full"
+                  alt="contact-us-page-graphic"
+                  src={ContactUsPageGraphic}
+                />
+              </div>
+            </div>
         </div>
 
       </motion.div>
